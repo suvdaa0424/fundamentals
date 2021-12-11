ef2fdabb04d90ab3aa0c5deabdc0c4122bb1e529
suvdaa0424 <85181506+suvdaa0424@users.noreply.github.com>
2021-12-11 13:23:21 -0600

    12-06

---
+++ script.js
@@ -0,0 +1,55 @@
+// const hello = "Hello world"
+
+
+
+// console.log('hi')
+
+// function helloWorld() {
+//     console.log(myCodeName)
+// }
+// helloWorld()
+// console.log(2+2)
+// let cat = 'dog'
+// console.log(cat)
+// console.log(hello)
+// let myCodeName = 'top secret'
+// helloWorld()
+
+// let aString = "Hihi";
+// let aNumber = 42;
+// console.log(Number(aString) + 42)
+// let isTrue = true
+// console.log(isTrue == (2+2 == 4))
+// console.log(isTrue == 0)
+// const anArray = ["1", 2, 3,, "A string", isTrue]
+const anObject = {
+    firstName: "Zaphod",
+    lastName: "Skywalker",
+    bestFriend: "Yoda",
+}
+anObject["another key"] = "500"
+anObject.friend = "Zach"
+console.log(anObject)
+// console.log(anObject.firstName)
+
+// const firstNumber = 23
+// let secondNumber = 19
+// const theAnswer = firstNumber + secondNumber
+// console.log(theAnswer)
+// secondNumber = 100
+// console.log(theAnswer)
+// const num1 = 1
+// const num2 = 2
+// let answer = num1 + num2
+
+// function add(num1, num2) {
+//     console.log(answer)
+// }
+// add()
+function printNumbers() {
+    for (let i = 1; i <= 10; i++) {
+        console.log(i);
+    }
+}
+
+printNumbers()
